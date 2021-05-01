b0df2bf9b52369efc66e8ed8c42d04d1aff07290
Gian <gpecile@itba.edu.ar>
2021-05-01 13:26:09 -0300

    Added Vuelidate Integration and implemented it in LogIn view.

--- src/main.js
+++ src/main.js
@@ -2,13 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import VueRouter from 'vue-router';
+import Vuelidate from 'vuelidate'
 
 // import { Plugin } from 'vue-responsive-video-background-player';
+Vue.use(Vuelidate);
 
 import Home from "@/views/Home";
 import Workouts from "@/views/Workouts";
 import Profile from "@/views/Profile";
 import Routines from "@/views/Routines";
+import LogIn from "./views/LogIn";
 
 // import {Routine} from "./store/RoutineStore";
 import RoutineDetail from "@/views/RoutineDetail";
@@ -20,11 +23,12 @@ Vue.config.productionTip = false
 
 const router = new VueRouter({
     routes: [
-        { path: '/', component: Home},
-        { path: '/Workouts', component: Workouts},
-        { path: '/Profile', component: Profile},
-        { path: '/Routines', component: Routines},
-        { path: '/RoutineDetail', component: RoutineDetail, name: 'RoutinePath', props: true},
+        {path: '/', component: Home},
+        {path: '/Workouts', component: Workouts},
+        {path: '/Profile', component: Profile},
+        {path: '/Routines', component: Routines},
+        {path: '/LogIn', component: LogIn},
+        {path: '/RoutineDetail', component: RoutineDetail, name: 'RoutinePath', props: true},
     ]
 })
 
---
+++ src/views/LogIn.vue
@@ -0,0 +1,73 @@
+<template>
+  <div>
+
+    <v-card class="text-center ma-3 px-3 py-4" elevation="2" tile>
+      <v-card-text class="text-center title">Welcome!</v-card-text>
+
+      <v-form>
+        <v-text-field
+            @blur="$v.username.$touch()"
+            v-model="username"
+            label="Username"
+            outlined
+            hint="Username must be at least 3 characters long."
+            append-icon="mdi-account"
+        ></v-text-field>
+        <!--      <div>{{$v}}</div>-->
+        <v-text-field
+            @blur="$v.password.$touch()"
+            v-model="password"
+            :append-icon="show ? 'mdi-eye' : 'mdi-eye-off'"
+            :type="show ? 'text' : 'password'"
+            label="Password"
+            hint="Password must be at least 8 characters long."
+            outlined
+            counter
+            @click:append="show = !show"
+        ></v-text-field>
+        <v-btn color="teal" class="mt-3" large width="100%" @click="processInput" dark>Log in</v-btn>
+      </v-form>
+
+    </v-card>
+  </div>
+</template>
+
+<script>
+import {required, maxLength, minLength} from 'vuelidate/lib/validators'
+
+export default {
+  name: "LogIn",
+  data() {
+    return {
+      show: false,
+      password: '',
+      username: ''
+    }
+  },
+  methods: {
+    // Métodos que acceden a la API
+    processInput() {
+      // do something.
+    }
+  },
+  validations: {
+    //  Conexion con Vuelidate.
+    username: {
+      required: required,
+      minLength: minLength(3),
+      maxLength: maxLength(15)
+    },
+    password: {
+      required: required,
+      minLength: minLength(8)
+    }
+  },
+  computed: {
+    //  Conexion con la API.
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
