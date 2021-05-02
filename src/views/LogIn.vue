2bcadc92a33c35968412ec1a1b9546ea17dcce84
Gian <gpecile@itba.edu.ar>
2021-05-02 15:41:02 -0300

    Implemented AccountData component without API integration, added to Profile View.

--- src/views/LogIn.vue
+++ src/views/LogIn.vue
@@ -1,39 +1,46 @@
 <template>
   <div>
+    <v-row align="center" justify="space-between">
+      <v-col cols="6">
+        <!--        <CAccData></CAccData>-->
+      </v-col>
+      <v-col cols="6">
+        <v-card class="text-center ma-3 px-3 py-4" elevation="3" tile>
+          <v-card-title class="title">Welcome!</v-card-title>
 
-    <v-card class="text-center ma-3 px-3 py-4" elevation="2" tile>
-      <v-card-text class="text-center title">Welcome!</v-card-text>
+          <v-form>
+            <v-text-field
+                @blur="$v.username.$touch()"
+                v-model="username"
+                label="Username"
+                outlined
+                hint="Username must be at least 3 characters long."
+                append-icon="mdi-account"
+            ></v-text-field>
+            <!--      <div>{{$v}}</div>-->
+            <v-text-field
+                @blur="$v.password.$touch()"
+                v-model="password"
+                :append-icon="show ? 'mdi-eye' : 'mdi-eye-off'"
+                :type="show ? 'text' : 'password'"
+                label="Password"
+                hint="Password must be at least 8 characters long."
+                outlined
+                counter
+                @click:append="show = !show"
+            ></v-text-field>
+            <v-btn color="teal" class="mt-3" large width="100%" @click="processInput" dark>Log in</v-btn>
+          </v-form>
 
-      <v-form>
-        <v-text-field
-            @blur="$v.username.$touch()"
-            v-model="username"
-            label="Username"
-            outlined
-            hint="Username must be at least 3 characters long."
-            append-icon="mdi-account"
-        ></v-text-field>
-        <!--      <div>{{$v}}</div>-->
-        <v-text-field
-            @blur="$v.password.$touch()"
-            v-model="password"
-            :append-icon="show ? 'mdi-eye' : 'mdi-eye-off'"
-            :type="show ? 'text' : 'password'"
-            label="Password"
-            hint="Password must be at least 8 characters long."
-            outlined
-            counter
-            @click:append="show = !show"
-        ></v-text-field>
-        <v-btn color="teal" class="mt-3" large width="100%" @click="processInput" dark>Log in</v-btn>
-      </v-form>
-
-    </v-card>
+        </v-card>
+      </v-col>
+    </v-row>
   </div>
 </template>
 
 <script>
-import {required, maxLength, minLength} from 'vuelidate/lib/validators'
+import {required, maxLength, minLength} from 'vuelidate/lib/validators';
+// import AccountData from "../components/AccountData";
 
 export default {
   name: "LogIn",
@@ -44,6 +51,10 @@ export default {
       username: ''
     }
   },
+  components:
+      {
+        // CAccData: AccountData,
+      },
   methods: {
     // Métodos que acceden a la API
     processInput() {
--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,58 +1,56 @@
 <template>
-    <div>
-        <h1>profile</h1>
-        <v-row>
-            <v-col class="pl-8" cols="1">
-                <v-avatar color="teal" class="profile" size="164" ></v-avatar>
-            </v-col>
-            <v-col>
-                <h1 class="pt-14 pl-10">{{name + ' ' + surname }}</h1>
-            </v-col>
-        </v-row>
+  <div>
+<!--    <h1>profile</h1>-->
+    <v-row class="pa-4">
+      <v-col class="ml-8" cols="1">
+        <v-avatar color="teal" class="profile" size="164"></v-avatar>
+      </v-col>
+      <v-col>
+        <h1 class="pt-14 pl-15">{{ name + ' ' + surname }}</h1>
+      </v-col>
+    </v-row>
 
-        <v-row>
-            <v-col cols="6" class="pa-8">
-                <v-card outlined>
-                    <v-card-title>Personal Data</v-card-title>
+    <v-row>
+      <v-col cols="6" class="pa-8">
+        <CAccData></CAccData>
+      </v-col>
 
-                </v-card>
-            </v-col>
+      <v-col cols="6" class="pa-8">
+        <v-card outlined>
+          <v-card-title>Favorite Routines</v-card-title>
+          <c-favorites></c-favorites>
+        </v-card>
+      </v-col>
+    </v-row>
 
-            <v-col cols="6" class="pa-8">
-                <v-card outlined>
-                    <v-card-title>Favorite Routines</v-card-title>
-                    <c-favorites></c-favorites>
-                </v-card>
-            </v-col>
-        </v-row>
-
-                    <!--                    <img :src="user.photo">-->
+    <!--                    <img :src="user.photo">-->
 
 
-
-<!--        <v-btn v-for="option in buttonsInfo" :key="option.name" :to="option.route" class="teal text-center" rounded dark>-->
-<!--            {{ option.name }}-->
-<!--        </v-btn>-->
-    </div>
+    <!--        <v-btn v-for="option in buttonsInfo" :key="option.name" :to="option.route" class="teal text-center" rounded dark>-->
+    <!--            {{ option.name }}-->
+    <!--        </v-btn>-->
+  </div>
 </template>
 
 <script>
 import Favorites from "../components/Favorites";
+import AccountData from "../components/AccountData";
 
 export default {
-    name: "Profile",
+  name: "Profile",
 
-    components: {
-        CFavorites: Favorites,
-    },
+  components: {
+    CFavorites: Favorites,
+    CAccData: AccountData
+  },
 
-    data: () => ({
-        name: 'Juan',
-        surname:'Perez',
-        age: '',
-        country:'',
-        photo: './assets/logo.png',
-    }),
+  data: () => ({
+    name: 'Juan',
+    surname: 'Perez',
+    age: '',
+    country: '',
+    photo: './assets/logo.png',
+  }),
 }
 </script>
 
